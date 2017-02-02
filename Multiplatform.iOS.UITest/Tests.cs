8f7899c485b848605fcfe94a9a5795d9991d255b
Krisztián Gödrei <godreikrisztian@gmail.com>
2017-02-02 17:52:29 +0100

    Uitest (#2)
    * xamarin ios uitest
    
    * x86 arch for ios debug config
    
    * code style

---
+++ Multiplatform.iOS.UITest/Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.iOS;
+
+namespace Multiplatform.iOS.UITest
+{
+	[TestFixture]
+	public class Tests
+	{
+		iOSApp app;
+
+		[SetUp]
+		public void BeforeEachTest()
+		{
+			string appBundlePath = Environment.GetEnvironmentVariable("APP_BUNDLE_PATH");
+
+			if (!string.IsNullOrEmpty(appBundlePath))
+			{
+				// In case of Bitrise step: steps-xamarin-ios-uitest.
+				app = ConfigureApp
+					.iOS
+					.AppBundle(appBundlePath)
+					.StartApp();
+			}
+			else {
+				// In case of Bitrise step: steps-xamarin-testcloud-for-ios or running in the IDE.
+				app = ConfigureApp
+					.iOS
+					.StartApp();
+			}
+		}
+
+		[Test]
+		public void AppLaunches()
+		{
+			app.Screenshot("First screen.");
+		}
+	}
+}
