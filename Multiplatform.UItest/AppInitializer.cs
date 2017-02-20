9be9de26817bcc4c3914d9a10be0e23af20df7d0
Krisztián Gödrei <godreikrisztian@gmail.com>
2017-02-20 14:16:37 +0100

    Cross platform uitest (#4)
    * cross platform uitest
    
    * android-uitest

---
+++ Multiplatform.UItest/AppInitializer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.UITest;
+using Xamarin.UITest.Configuration;
+
+namespace Multiplatform.UItest
+{
+	public class AppInitializer
+	{
+		public static IApp StartApp(Platform platform)
+		{
+			if (platform == Platform.Android)
+			{
+				AndroidAppConfigurator androidConfigurator = ConfigureApp.Android;
+
+				string apkPath = Environment.GetEnvironmentVariable("ANDROID_APK_PATH");
+				if (!string.IsNullOrEmpty(apkPath))
+				{
+					androidConfigurator.ApkFile(apkPath);
+				}
+
+				string emulatorSerial = Environment.GetEnvironmentVariable("BITRISE_EMULATOR_SERIAL");
+				if (!string.IsNullOrEmpty(emulatorSerial))
+				{
+					androidConfigurator.DeviceSerial(emulatorSerial);
+				}
+
+				return androidConfigurator.StartApp();
+			}
+
+			iOSAppConfigurator iosConfigurator = ConfigureApp.iOS;
+
+			string appBundlePath = Environment.GetEnvironmentVariable("APP_BUNDLE_PATH");
+			if (!string.IsNullOrEmpty(appBundlePath))
+			{
+				iosConfigurator.AppBundle(appBundlePath);
+			}
+
+			string simulatorUDID = Environment.GetEnvironmentVariable("IOS_SIMULATOR_UDID");
+			if (!string.IsNullOrEmpty(simulatorUDID))
+			{
+				iosConfigurator.DeviceIdentifier(simulatorUDID);
+			}
+
+			return iosConfigurator.StartApp();
+		}
+	}
+}
