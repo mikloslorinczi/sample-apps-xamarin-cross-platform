5c60956fe7c0f242965413786a03d019f5c8a1bc
Krisztian Godrei <godreikrisztian@gmail.com>
2017-06-27 15:07:59 +0200

    nunit test project

---
+++ Multiplatform.NunitTest/Test.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+namespace Multiplatform.NunitTest
+{
+	[TestFixture]
+	public class TestsSample
+	{
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+
+		[TearDown]
+		public void Tear()
+		{
+		}
+
+		[Test]
+		public void Pass()
+		{
+			Assert.True(true);
+		}
+
+		[Test]
+		[Ignore("another time")]
+		public void Ignore()
+		{
+			Assert.True(false);
+		}
+
+		[Test]
+		public void Inconclusive()
+		{
+			Assert.Inconclusive("Inconclusive");
+		}
+	}
+}
